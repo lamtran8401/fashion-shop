209584d3bc199eb2c1f578913d2152978f8e6a8e
tranvulamdev <tranvulam.dev@gmail.com>
2023-03-11 15:09:26 +0700

    update src v3

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState({})
   return (
     <AuthContext.Provider value={[currentUser, setCurrentUser]}>
       {React.Children.only(children)}
