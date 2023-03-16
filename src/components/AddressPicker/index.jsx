74e692b1e304b77d85f473af342aaabf4b838d68
tranvulamdev <tranvulam.dev@gmail.com>
2023-03-16 14:24:47 +0700

    change code to deploy

--- src/components/AddressPicker/index.jsx
+++ src/components/AddressPicker/index.jsx
@@ -1,12 +1,12 @@
 import address from '@/data/address'
 import { Cascader } from 'antd'
 
-const AddressPicker = () => {
+const AddressPicker = ({ placeholder = 'Please select your address...' }) => {
   return (
     <Cascader
       showSearch
       options={address}
-      placeholder='Please select your address...'
+      placeholder={placeholder}
       style={{ width: 'max-content' }}
     />
   )
